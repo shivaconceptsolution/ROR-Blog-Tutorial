c151b718c6f34b0a2a402ee798153fc3c17b8219
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-06 16:08:55 +0530

    Add files via upload

--- controllers/welcome_controller.rb
+++ controllers/welcome_controller.rb
@@ -3,15 +3,28 @@ class WelcomeController < ApplicationController
   	if params[:btnsubmit]!=nil
   		uid = params[:txtuser]
   		pwd = params[:txtpass]
-  		if uid=="admin" and pwd=="123"
+  		@data=Register.where("email = ? AND password = ?",uid,pwd).take
+  		if @data!=nil
   			redirect_to "/articles"
   	    else
   	       @error = "invalid userid and password" 		
-
-
-  		end	
-
+        end	
     end
-    		
-  end
+   end
+  def reg
+  	    if params[:btnsubmit]!=nil 
+        email = params[:txtemail]
+  		pwd = params[:txtpass]  
+  		fname = params[:txtfname]
+  		mobile = params[:txtmobile] 
+  		@r = Register.new()
+  		@r.email=email
+  		@r.password=pwd
+  		@r.fname=fname
+  		@r.mobile=mobile
+  		@r.save
+  		redirect_to "/welcome/index"
+  	    end
+
+end 	
 end
